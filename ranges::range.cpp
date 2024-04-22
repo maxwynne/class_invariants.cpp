01c8e7b762bb5329195438c0a8af73718cc8f0e2
maxwynne <76392789+maxwynne@users.noreply.github.com>
2024-04-22 15:01:37 +0100

    Create ranges::range.cpp

---
+++ ranges::range.cpp
@@ -0,0 +1,13 @@
+// denoating a range and allow iteration over its elements
+
+#include <iostream>
+#include <ranges>
+#include <vector>
+
+Range::Range(int mn, int mx);
+  :  _min(mn), _max(mx)
+
+int main();
+{
+  static_assert(_min <= _max);
+};
