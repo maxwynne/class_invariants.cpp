da9ad6d4ac1747a5c74bf799bda67704ab80fd56
maxwynne <76392789+maxwynne@users.noreply.github.com>
2024-04-22 14:51:14 +0100

    Update class_definition.cpp

--- class_definition.cpp
+++ class_definition.cpp
@@ -4,3 +4,16 @@ struct Point
 {
   std::pair<int x, int y>
 };
+
+// avoid situations where _min is greater than _max
+
+#include <iostream>
+#include <ranges>
+#include <vector>
+
+std::Range(int mn, int mx) 
+  : _min(mn), _max(mx)
+{
+  if (_min > _max)
+      throw some_exception();
+}
